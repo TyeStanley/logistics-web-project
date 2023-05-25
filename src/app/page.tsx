89218d81dc1c8793d495034679696196267aed12
Tye Stanley <tyestanley090@gmail.com>
2023-05-24 22:12:29 -0700

    added font folder with poppins, added poppins font to text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen">
+    <main className="h-screen w-screen overflow-x-hidden">
       <Navbar />
       <Hero />
       <div>text</div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,13 +4,13 @@ import heroImage from "../assets/heroImage.jpg";
 export default function Hero() {
   return (
     <section className="flex h-screen items-center justify-center bg-[url('../assets/heroImage.jpg')] bg-cover bg-center">
-      <div className="flex h-[250px] w-[500px] flex-col items-center justify-center rounded-xl bg-gray-300/80 shadow-lg shadow-gray-700">
-        <h1 className="text-xl font-extrabold text-blue-700">Lorem ipsum</h1>
-        <p className="my-4 text-center">
+      <div className="flex h-[250px] w-[500px] flex-col items-center justify-center rounded-xl bg-gray-300/90 shadow-lg shadow-gray-300 ">
+        <h1 className="text-2xl font-extrabold text-gray-900">Lorem ipsum</h1>
+        <p className="my-4 px-6 text-center text-gray-800">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt aut
           facilis commodi esse maiores voluptate laborum rerum blanditiis?
         </p>
-        <button className="rounded bg-green-700 p-2 text-white">
+        <button className="mt-5 rounded bg-blue-700 p-2 text-gray-100">
           Quote Now
         </button>
       </div>

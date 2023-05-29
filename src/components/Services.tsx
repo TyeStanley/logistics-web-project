6a285b66967a174e66747619f30c004d5310385d
Tye Stanley <tyestanley090@gmail.com>
2023-05-29 11:35:56 -0700

    added svg icons and mapped services section

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -11,7 +11,7 @@ export default function Services() {
   return (
     <section
       id="services"
-      className="relative h-[700px] w-full pt-[150px]"
+      className="relative h-[700px] w-full pt-[100px]"
     >
       <h1 className="text-center text-3xl font-bold text-gray-800">
         Logistic Services
@@ -20,14 +20,14 @@ export default function Services() {
         {servicesJson.map(({ image, title, content }: serviceType) => (
           <div
             key={title}
-            className="new-shadow flex h-[300px] w-[200px] flex-col items-center justify-self-center rounded bg-gray-100 p-5"
+            className="new-shadow flex flex-col items-center justify-self-center rounded bg-gray-100 p-5"
           >
             <Image
-              src={image}
+              src={`/svg-icons/${image}`}
               alt={title}
               width={150}
               height={150}
-              className="rounded-full border-2 border-red-500 bg-gray-500"
+              className=""
             />
             <h2 className="text-xl font-bold text-gray-800">{title}</h2>
             <p className="text-gray-700">{content}</p>

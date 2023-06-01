5b99545dee30049cd799bb2992848ac192acad71
Tye Stanley <tyestanley090@gmail.com>
2023-06-01 16:23:47 -0700

    added mobile nav burger with click animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,24 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "../assets/Goldentail_Logistics_LLC.png";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+
+    document.getElementById("top-bar")!.classList.toggle("opacity-0");
+    document.getElementById("middle-bar")!.classList.toggle("rotate-45");
+    document.getElementById("middle-bar2")!.classList.toggle("-rotate-45");
+    document.getElementById("bottom-bar")!.classList.toggle("opacity-0");
+  };
+
   return (
-    <section className="fixed z-50 flex w-[calc(100%-17px)] justify-around bg-gray-800 py-3">
+    <section className="fixed z-50 flex w-full justify-between bg-gray-800 px-3 py-3 lg:w-[calc(100%-17px)] lg:justify-around">
       <Image
         src={logo}
         alt="Goldentail Logistics LLC Logo"
@@ -12,7 +26,31 @@ export default function Navbar() {
         height={125}
       />
 
-      <ul className="flex items-center gap-5 text-white">
+      {/* mobile nav */}
+      <div
+        onClick={toggleMenu}
+        className="my-auto flex h-[30px] flex-col justify-between lg:hidden"
+      >
+        <div
+          id="top-bar"
+          className="h-[5px] w-10 rounded bg-gray-100"
+        />
+        <div
+          id="middle-bar"
+          className="h-[5px] w-10 translate-y-[4px] rounded bg-gray-100"
+        />
+        <div
+          id="middle-bar2"
+          className="h-[5px] w-10 -translate-y-[4px] rounded bg-gray-100"
+        />
+        <div
+          id="bottom-bar"
+          className="h-[5px] w-10 rounded bg-gray-100"
+        />
+      </div>
+
+      {/* desktop nav */}
+      <ul className="hidden items-center gap-5 text-white lg:flex">
         <li>
           <a
             href="#about"

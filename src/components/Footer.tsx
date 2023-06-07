06cd6b6aad8b76c2dd31a299e6d3980294288d9d
Tye Stanley <tyestanley090@gmail.com>
2023-06-07 10:56:42 -0700

    updated footer and added mobile reponsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,8 +4,8 @@ import logo from "../assets/Goldentail_Logistics_LLC.png";
 
 export default function Footer() {
   return (
-    <section className="mt-[150px] flex w-full justify-center bg-gray-800 p-5">
-      <div className="flex flex-col items-center justify-center">
+    <section className="mt-[150px] flex w-full flex-wrap justify-around bg-gray-800 p-5">
+      <div className="flex flex-col justify-center">
         <Link href="/">
           <Image
             src={logo}
@@ -14,14 +14,42 @@ export default function Footer() {
             height="150"
           />
         </Link>
-        <p className="mt-5 text-white">
+        <p className="mt-5 text-[12px] text-white">
           © 2023 Goldentail Logistics LLC. All rights reserved.
         </p>
       </div>
 
+      <div className="flex flex-col items-center justify-center p-5">
+        <div className="grid grid-cols-2 gap-x-12 gap-y-2">
+          <a
+            href="#about"
+            className="text-white"
+          >
+            About
+          </a>
+          <a
+            href="#services"
+            className="text-white"
+          >
+            Services
+          </a>
+          <a
+            href="#career"
+            className="text-white"
+          >
+            Career
+          </a>
+          <a
+            href="#contact"
+            className="text-white"
+          >
+            Contact
+          </a>
+        </div>
+      </div>
+
       <div className="p-5">
-        <h1 className="text-center text-3xl text-white">Social Media</h1>
-        <div className="flex justify-center">
+        <div className="mb-1 flex items-center justify-center">
           <Link href="https://www.linkedin.com/in/tye-stanley/">
             <Image
               src="/svg-icons/linkedin.svg"
@@ -31,23 +59,39 @@ export default function Footer() {
               className="m-2 cursor-pointer"
             />
           </Link>
-          <Link href="https://github.com/TyeStanley">
+          <Link href="https://tyestanley.com/">
             <Image
-              src="/svg-icons/github.svg"
+              src="/svg-icons/instagram.svg"
               alt="LinkedIn"
-              width="25"
-              height="25"
+              width="30"
+              height="30"
+              className="m-2 cursor-pointer"
+            />
+          </Link>
+          <Link href="https://tyestanley.com/">
+            <Image
+              src="/svg-icons/facebook.svg"
+              alt="LinkedIn"
+              width="29"
+              height="29"
+              className="m-2 cursor-pointer"
+            />
+          </Link>
+          <Link href="https://tyestanley.com/">
+            <Image
+              src="/svg-icons/twitter.svg"
+              alt="LinkedIn"
+              width="29"
+              height="29"
               className="m-2 cursor-pointer"
             />
           </Link>
         </div>
-      </div>
-
-      <div className="flex flex-col items-center justify-center p-5">
-        <h1 className="text-3xl text-white">Address</h1>
-        <p className="text-white">Goldentail Logistics LLC</p>
-        <p className="text-white">1234 Main St</p>
-        <p className="text-white">Las Vegas, NV 89001</p>
+        <div className="p-2">
+          <p className="text-[12px] text-white">Goldentail Logistics LLC</p>
+          <p className="text-[12px] text-white">1234 Main St</p>
+          <p className="text-[12px] text-white">Las Vegas, NV 89001</p>
+        </div>
       </div>
     </section>
   );
